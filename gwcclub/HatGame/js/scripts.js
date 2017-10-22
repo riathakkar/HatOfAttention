2bdde06668d936c96d6ba3d985561131bfc028f6
riathakkar <riathakkar16@gmail.com>
2017-10-22 10:59:52 -0700

    Update scripts.js

--- gwcclub/HatGame/js/scripts.js
+++ gwcclub/HatGame/js/scripts.js
@@ -1,8 +1,14 @@
 var data = [{
-    question:"What is pwd",
-    answer: "Gives your current path"
-},{ question:"What is cd",
-    answer: "A calling directory"
+    question:"output of 3 +"3"",
+    answer: "33"
+},{ question:"a = 1; b=2; c= a*b; output of print(c)?",
+    answer: "2"
+  },{ question:"x = 9 \n x = "10" \n print(x). Output?"
+    answer: "10"
+  },{ question:"What is the type of "7""
+    answer: "string"
+  },{ question:"What is true"
+    answer: "a boolean expression"
   }];
 var num = Math.round(Math.random()*(data.length-1));
 function searchItem(){
